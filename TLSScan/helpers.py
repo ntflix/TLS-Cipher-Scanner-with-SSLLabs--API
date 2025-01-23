f36c0f0a0e11672e8b2e20e7feaeac47141afa2e
ntflix <felix@iron59.co.uk>
2025-01-23 18:13:12 +0000

    get rid of IPs in scanner input

--- TLSScan/helpers.py
+++ TLSScan/helpers.py
@@ -1,4 +1,41 @@
 import re
+import requests
+import json
+import ipaddress
+
+
+SVCXPLORE_BASE_URL = "http://10.244.69.163/api/interfaces"
+
+
+def get_hosts_from_svcxplore():
+    domains: list[str] = []
+
+    response = requests.get(SVCXPLORE_BASE_URL)
+    if response.status_code == 200:
+        interfaces = json.loads(response.text)
+        for interface in interfaces:
+            interface: str
+            try:
+                interface: str = interface["url"]  # type: ignore
+            except KeyError:
+                pass
+            domains.append(interface)
+    else:
+        print(f"Failed to get hosts from svcxplore: {response.text}")
+
+    domains = [get_domain_only(domain) for domain in domains]
+    domains = list(set(domains))  # Remove duplicates
+    domains = [domain for domain in domains if not is_ip(domain) is False]  # Remove IPs
+
+    return domains
+
+
+def is_ip(host: str) -> bool:
+    try:
+        ipaddress.ip_address(host)
+        return True
+    except ValueError:
+        return False
 
 
 def get_domain_only(host: str) -> str:
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 from TLSScan import scan_with_ssllabs
-from TLSScan.get_hosts_from_svcxplore import get_hosts_from_svcxplore
-from TLSScan.helpers import get_domain_only, get_hosts
+from TLSScan.helpers import get_hosts, get_hosts_from_svcxplore
 
 OUTPUT_FILENAME = "Output/tlsscan_new.csv"
 
@@ -15,8 +14,6 @@ def get_list_of_hosts():
 
     hosts = hosts_from_file + hosts_from_svcxplore
 
-    hosts = [get_domain_only(host) for host in hosts]
-    hosts = list(set(hosts))  # Remove duplicates
     return hosts
 
 
