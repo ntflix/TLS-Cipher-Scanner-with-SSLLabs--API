3a9df9067b607ac0c6b8e3c221d7ed49c1bb9736
ntflix <felix@iron59.co.uk>
2025-01-23 10:20:44 +0000

    Improve error handling and retry logic in TLSScan

--- TLSScan/tlsscan_ssllabs.py
+++ TLSScan/tlsscan_ssllabs.py
@@ -1,7 +1,9 @@
 import socket
+from httpx import HTTPStatusError
 from ssllabs.api import Endpoint, Analyze
 from ssllabs.data.host import HostData
 from ssllabs.data.endpoint import EndpointData
+import asyncio
 
 
 def get_ip_from_host(host: str) -> str:
@@ -11,21 +13,47 @@ def get_ip_from_host(host: str) -> str:
 
 async def get_endpoint(target: str) -> EndpointData:
     api = Endpoint()
-    endpoint: EndpointData = await api.get(
-        host=target,
-        s=get_ip_from_host(target),
-        fromCache=True,
-        # all=True,
-    )
+    endpoint: EndpointData | None = None
+    try_again: bool = False
+
+    while try_again:
+        try_again = False
+        try:
+            endpoint = await api.get(
+                host=target,
+                s=get_ip_from_host(target),
+                fromCache=True,
+                # all=True,
+            )
+        except HTTPStatusError as e:
+            # if 429, wait for 10 seconds and retry
+            if e.response.status_code == 429:
+                try_again = True
+                await asyncio.sleep(10)
+
+    assert endpoint is not None
     return endpoint
 
 
 async def analyze(target: str) -> HostData:
     api = Analyze()
-    host: HostData = await api.get(
-        host=target,
-        fromCache=True,
-        all="on",
-        maxAge=168,
-    )
+    host: HostData | None = None
+    try_again: bool = False
+
+    while try_again:
+        try_again = False
+        try:
+            host = await api.get(
+                host=target,
+                fromCache=True,
+                all="on",
+                maxAge=168,
+            )
+        except HTTPStatusError as e:
+            # if 429, wait for 10 seconds and retry
+            if e.response.status_code == 429:
+                try_again = True
+                await asyncio.sleep(10)
+
+    assert host is not None
     return host
